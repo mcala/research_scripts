23d1d7b5115a66b9eb1c4d94024a5b8d62e96c77
Andrew McAllister <mcala@umich.edu>
2015-09-08 11:54:32 -0700

    Added folder for phonon assisted Auger recombination scripts. Only directly transfered the "after_phonon" script, will gradually adding in scripts as you get them up to speed with the new code.

---
+++ phonon_auger/utilities/after_phonon.zsh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+if [[ $1 = "" ]]; then
+  RUN_NAME='last_run'
+else
+  RUN_NAME=$1
+fi
+
+if [ -d ${RUN_NAME} ]; then
+  echo "Folder Already Exists."
+  exit 1
+fi
+
+#After an Auger run, move all the large unwieldy names to their own directory. In addition, move plotting data to it's own directory where it is renamed to something more convienent.
+mkdir data/${RUN_NAME}
+mkdir data/${RUN_NAME}/plotting
+mkdir data/${RUN_NAME}/bymode
+
+kpoints=`tail -1 ../nscf_kpt.in | awk '{print $1}'`
+
+for i in `seq 1 8`; do
+
+mv auger_coef_ep_eeh_absorptionemission000${i}.dat data/${RUN_NAME}
+mv auger_coef_ep_eeh_absorption_000${i}.dat data/${RUN_NAME}
+mv auger_coef_ep_eeh_absorptionemission_bymode000${i}.dat data/${RUN_NAME}
+mv auger_coef_ep_eeh_emission_000${i}.dat data/${RUN_NAME}
+
+cp data/${RUN_NAME}/auger_coef_ep_eeh_absorptionemission000${i}.dat data/${RUN_NAME}/plotting/${kpoints}_0${i}.dat
+cp data/${RUN_NAME}/auger_coef_ep_eeh_absorptionemission_bymode000${i}.dat data/${RUN_NAME}/bymode/${kpoints}_0${i}.dat
+
+done
+
+cp *.in data/${RUN_NAME}
+cp *.out data/${RUN_NAME}
+
